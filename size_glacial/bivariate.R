f029ef3a737c75220044418095f7e3d93475a4a2
--get <vsyverson@gmail.com>
2019-06-24 12:38:44 -0700

    bivariate works

--- size_glacial/bivariate.R
+++ size_glacial/bivariate.R
@@ -1,4 +1,15 @@
-require(paleoTS);require(xlsx)
-taxon<-"Felis_concolor"
+require(paleoTS);require(xlsx);require(Hotelling)
 path<-"Cats/"
-data<-read.xlsx(paste0(path,taxon,".xlsx"),sheetName="m1")[,c("Pit","Age","L","W")]
+taxon<-"Felis_concolor"
+meas<-"m1"
+data<-read.xlsx(paste0(path,taxon,".xlsx"),sheetName=meas)[,c("Pit","Age","L","W")]
+modern<-subset(data,data$Age==0)
+fossil<-subset(data,data$Age!=0)
+test<-hotelling.test(modern[,c("L","W")],fossil[,c("L","W")])
+plot(0,0,xlim=range(data$L),ylim=range(data$W),pch=0,xlab=paste(meas,"length"),ylab=paste(meas,"width"),main=paste(taxon,meas))
+  points(modern$L,modern$W,pch=1);points(mean(modern$L),mean(modern$W),pch=16)
+  points(fossil$L,fossil$W,pch=2);points(mean(fossil$L),mean(fossil$W),pch=17)
+  legend("bottomright",legend=c("modern","modern mean","fossil","fossil mean"),pch=c(1,16,2,17),cex=0.9)
+  legend("topleft",legend=c(paste("Hotelling T2",capture.output(test)[1]),capture.output(test)[4]),bty="n",cex=0.9)
+  
+  
